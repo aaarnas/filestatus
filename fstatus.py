a935cc8790d93045671e80148ec50cd7f14bf347
aaarnas <aaarnui@gmail.com>
2014-04-07 23:44:47 +0300

    Add words counting. Printing list of most used words

--- fstatus.py
+++ fstatus.py
@@ -1,11 +1,38 @@
 import os
 import sys
 
+def countWords(filepath, words_dict):
+    with open(filepath, 'r') as file:
+        for line in file:
+            for word in line.split():
+                word = word.lower()
+                if word in words_dict:
+                    words_dict[word] += 1
+                else:
+                    words_dict[word] = 1
+
+
 def printPathStatus(path):
     print "Path '%s' information:" % (path)
-    dir_list = os.listdir(path)
-    for file in dir_list:
-        print file
+    file_path = ""
+    words_dict = {}
+    for file in os.listdir(path):
+        file_path = path
+        if not path.endswith('/'):
+            file_path += "/"
+        file_path += file
+        if os.path.isfile(file_path):
+            countWords(file_path, words_dict)
+
+    print "Most common words in '%s'" % (path)
+    words_list = sorted(words_dict.items(), key=lambda x:x[1], reverse=True)
+    i = 0
+    for item in words_list:
+        i += 1
+        if i > 10:
+            break
+        print "%s - %d" % (item[0], item[1])
+
 
 if len(sys.argv) > 1:
     if os.path.isdir(sys.argv[1]):
