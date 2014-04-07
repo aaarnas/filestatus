6c2581aa61b54bb3e4d0839c4d0ec4b8251a04c6
aaarnas <aaarnui@gmail.com>
2014-04-08 00:09:27 +0300

    Add letters counting

--- fstatus.py
+++ fstatus.py
@@ -1,7 +1,7 @@
 import os
 import sys
 
-def countWords(filepath, words_dict):
+def countFileData(filepath, words_dict, letters_dict):
     with open(filepath, 'r') as file:
         for line in file:
             for word in line.split():
@@ -10,24 +10,41 @@ def countWords(filepath, words_dict):
                     words_dict[word] += 1
                 else:
                     words_dict[word] = 1
+                
+                for letter in word:
+                    if letter in letters_dict:
+                        letters_dict[letter] += 1
+                    else:
+                        letters_dict[letter] = 1
 
 
 def printPathStatus(path):
     print "Path '%s' information:" % (path)
     file_path = ""
     words_dict = {}
+    letters_dict = {}
     for file in os.listdir(path):
         file_path = path
         if not path.endswith('/'):
             file_path += "/"
         file_path += file
         if os.path.isfile(file_path):
-            countWords(file_path, words_dict)
+            countFileData(file_path, words_dict, letters_dict)
 
     print "Most common words in '%s'" % (path)
     words_list = sorted(words_dict.items(), key=lambda x:x[1], reverse=True)
     i = 0
     for item in words_list:
+        i += 1
+        if i > 10:
+            break
+        print "%s - %d" % (item[0], item[1])
+
+    print "Most common letters in '%s'" % (path)
+    letters_list = sorted(letters_dict.items(),
+            key=lambda x:x[1], reverse=True)
+    i = 0
+    for item in letters_list:
         i += 1
         if i > 10:
             break
